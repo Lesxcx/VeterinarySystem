1a097ad8aa1757d8a154c28a03fb25cddab1c68f
Lesxcx <szydlowski.konrad1999@gmail.com>
2023-01-26 19:27:33 +0100

    creating fields for the patient class

--- src/main/java/org/VeterinarySystem/Patient.java
+++ src/main/java/org/VeterinarySystem/Patient.java
@@ -1,5 +1,9 @@
 package org.VeterinarySystem;
 
+import java.util.Date;
+
 public class Patient {
-    public String name;
+    private String name, owner, breed, issue;
+    private int age;
+    private Date date;
 }
